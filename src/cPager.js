8769bfa3a36f290264cc92d8a40d561d67c3c784
Christian Marienfeld <post@chrisland.de>
2015-10-07 01:48:19 +0200

    init

---
+++ src/cPager.js
@@ -0,0 +1,24 @@
+/**
+* Easy JS One-Page System Framework
+*
+* @class cPager
+* @version 0.1.0
+* @license MIT
+*
+* @author Christian Marienfeld post@chrisand.de
+*
+* ### Examples:
+*
+*
+* @return {Object} cStorage Object
+*
+* @api public
+*/
+ 
+ 
+function cPager(dbname, rootString) {
+	
+
+	return this;
+}
+
---
+++ test/spec/ToolSpec.js
@@ -0,0 +1,21 @@
+describe("cPager", function() {
+	
+	
+	beforeEach(function() {
+			
+	});
+
+	
+	it("init and return this", function() {
+	
+		var pager = new cPager();
+		
+		
+		expect(pager).toBeObject();
+	
+	});
+
+	
+	
+
+});
