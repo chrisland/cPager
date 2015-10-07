de7441a6879020b330025a964b7c3d6b4ecc42e5
christian marienfeld <post@chrisland.de>
2015-10-07 16:55:10 +0200

    init and WIP changeById

--- src/cPager.js
+++ src/cPager.js
@@ -10,15 +10,102 @@
 * ### Examples:
 *
 *
-* @return {Object} cStorage Object
+* @return {Object} cPager Object
 *
 * @api public
 */
  
  
-function cPager(dbname, rootString) {
+function cPager(param) {
+	
+	this._page;
+	this._lastopen = false;
+	this._opt = {
+		handler: 'pageBtn',
+		displayStyle: 'block',
+		offButton: 'pageBtnOffline',
+		container: 'page',
+		start: false
+	}
+	
+	if (param) {
+		for (var i in param) {
+			if(param.hasOwnProperty(i)){
+				this._opt[i] = param[i];
+			}
+		}
+	}
+
+	this._page = document.getElementById(this._opt.container);
+	
+	if (!this._page) {
+		throw new Error("missing main container #"+this._opt.container);
+		return false
+	}
 	
 
+	
+	if ( this._lastopen ) {
+		//this.changePageById(this._lastopen);
+	} else {
+		if (this._opt.start) {
+			this.changePageById(this._opt.start);
+		}
+	}
+
+	
 	return this;
 }
+
+
+
+cPager.prototype.changePageById = function (pageId, pageTask, pageContent) {
+	console.log('changePageById', pageId, pageTask, pageContent);
+	
+	var task = changeContent( null, pageTask, pageContent, pageId);
+	if (task) {
+/*
+		fadePageDom(pageId, function () {
+			if (typeof(task) === "function") {
+				task();
+			}
+			addBtnEventListener();
+			addPageHistory(pageId,pageTask,pageContent);
+		});
+		return true;
+		*/
+	}
+
+	//addBtnEventListener();
+	
+	return false;
+	
+	
+};
+
+
+function changeContent (e, task, content, pageId) {
+	
+/*
+	if (task == 'back') {
+		var history = getPageHistory();
+		var last = history[history.length-2];
+		if (!last) {
+			return false;
+		}
+		if (last.pageId) {
+			APS.page.changePageById(last.pageId,last.pageTask,last.pageContent);				
+			kickLastPageHistory(2);
+		}
+		return true;	
+	} else if (APS.task && APS.task[task]) {
+		return APS.task[task](pageId,content,e,$_page);
+	}
+*/
+	return true;
+}
+
+
+
+
 
--- test/spec/ToolSpec.js
+++ test/spec/ToolSpec.js
@@ -1,21 +1,269 @@
 describe("cPager", function() {
+		
+	
+	it("init should throw error if the div main container was not found (opt.container has default value)", function() {
+		
+		expect(function () {
+			var pager = new cPager();
+		}).toThrow(new Error("missing main container #page"));
+	
+	});
 	
 	
-	beforeEach(function() {
+	it("init should throw error if the div main container was not found (opt.container has set value)", function() {
+		
+		var newContainername = 'error';
+		expect(function () {
+			var pager = new cPager({
+				container: newContainername
+			});
+		}).toThrow(new Error("missing main container #"+newContainername));
+	
+	});
+	
+		
+		
+		
+	
+	
+	describe("init with dom", function() {
+		
+		var pageId = 'page';
+		
+		beforeEach(function() {
 			
-	});
-
-	
-	it("init and return this", function() {
-	
-		var pager = new cPager();
+			
+			var newDiv = document.createElement("div"); 
+			newDiv.id = pageId;
+			
+			document.body.appendChild(newDiv); 
+		});
 		
 		
-		expect(pager).toBeObject();
+		afterEach(function() {
+			
+			document.getElementById(pageId).remove();
+		});
+	
+		
+		
+		
+		
+		it("init should have set default options param (handler)", function() {
+		
+			var pager = new cPager();
+
+			expect(pager._opt.handler).toBe('pageBtn');
+		
+		});
+		
+		it("init should have set default options param (displayStyle)", function() {
+		
+			var pager = new cPager();
+
+			expect(pager._opt.displayStyle).toBe('block');
+		
+		});
+		
+		it("init should have set default options param (offButton)", function() {
+		
+			var pager = new cPager();
+
+			expect(pager._opt.offButton).toBe('pageBtnOffline');
+		
+		});
+		
+		it("init should have set default options param (container)", function() {
+		
+			var pager = new cPager();
+
+			expect(pager._opt.container).toBe('page');
+		
+		});
+		
+		it("init should have set default options param (start)", function() {
+		
+			var pager = new cPager();
+
+			expect(pager._opt.start).toBe(false);
+		
+		});
+		
+		
+		
+		it("init should set given init-param-obj ( {test:'test'} )", function() {
+		
+			var pager = new cPager({
+				test: 'test'
+			});
+
+			expect(pager._opt.test).toBe('test');
+		
+		});
+		
+		it("init should set given init-param-obj ( {handler:'test'} )", function() {
+		
+			var pager = new cPager({
+				handler: 'test'
+			});
+
+			expect(pager._opt.handler).toBe('test');
+		
+		});
+	
+
+
+		
+		
+		it("init should trow error if the main container is not correct (opt.container is wrong)", function() {
+			
+			var newContainername = 'paGe';
+			expect(function () {
+				var pager = new cPager({
+					container: newContainername
+				});
+			}).toThrow(new Error("missing main container #"+newContainername));
+		
+		});
+		
+		
+		
+		it("init should have set default param (_lastopen) to false", function() {
+		
+			var pager = new cPager();
+
+			expect(pager._lastopen).toBe(false);
+		
+		});
+		
+		it("init should return this if default container was found in dom", function() {
+		
+			var pager = new cPager();
+
+			expect(pager).toEqual(new cPager());
+		
+		});
+		
+		
+		
+		it("init should have set default param (_page) not to be empty", function() {
+		
+			var pager = new cPager();
+
+			expect(pager._page).not.toBe();
+		
+		});
+		
+	
+		
+			
+	
+		
+
+		
 	
 	});
+	
+	
+	/*
 
+	describe("open start page", function() {
+		
+		var pageId = 'page';
+		var pager;
+		
+		beforeEach(function() {
+			
+			
+			var newDiv = document.createElement("div"); 
+			newDiv.id = pageId;
+			
+			document.body.appendChild(newDiv); 
+			
+			
+			pager = new cPager({
+				start: 'home'
+			});
+			
+			
+			spyOn(pager, 'changePageById');
+			
+		});
+		
+		
+		afterEach(function() {
+			
+			document.getElementById(pageId).remove();
+		});
+	
+		
+		
+		
+		
+		it("init fire changePageById if opt.start is set and no _lastopen ", function() {
+		
+			
+
+			expect().toHaveBeenCalled();
+		
+		});
+	});
 	
 	
+*/
+
+
+
+	describe("changePageById()", function() {
+		
+		var pageId = 'page';
+		var pager;
+		
+		beforeEach(function() {
+			
+			
+			var newDiv = document.createElement("div"); 
+			newDiv.id = pageId;
+			
+			document.body.appendChild(newDiv); 
+			
+			
+			pager = new cPager({
+				start: 'home'
+			});
+			
+			
+			spyOn(pager, 'changePageById');
+			
+		});
+		
+		
+		afterEach(function() {
+			
+			document.getElementById(pageId).remove();
+		});
+
+		
+		
+		it("changePageById sould get be called ", function() {
+		
+			pager.changePageById();
+
+			expect(pager.changePageById).toHaveBeenCalled();
+		
+		});
+		
+		
+	});
+
+
+
+
+
+
+
+
+
+
 
 });
